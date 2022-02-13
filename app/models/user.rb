a0ed57cc1502065b7f7cfa3e19aa84212a73a452
hf-since2021 <hrk.fj.0911mon@gmail.com>
2022-02-13 09:14:38 +0900

    Userモデルにバリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "can consist of only half-width characters and must contain at least 1 letter and 1 numeric character" }
+
+  with_options presence: true do
+    validates :nickname
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'can consist of only full-width characters' } do
+      validates :last_name
+      validates :first_name
+    end
+    with_options format: { with: /\A[ァ-ヶー]+\z/, message: 'can consist of only katakana' } do
+      validates :last_name_reading
+      validates :first_name_reading
+    end
+    validates :birth_date
+  end
 end
