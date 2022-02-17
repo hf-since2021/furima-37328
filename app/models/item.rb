ba789a3d2afde7fcbc4e0b9a8d7b353d356aed22
hf-since2021 <hrk.fj.0911mon@gmail.com>
2022-02-17 13:44:33 +0900

    Itemモデルのバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,19 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :name
+    validates :information
+    validates :image
+  end
+
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :category_id
+    validates :sales_status_id
+    validates :shipping_fee_status_id
+    validates :prefecture_id
+    validates :scheduled_delivery_id
+    validates :price
+  end
+
   belongs_to :user
   has_one_attached :image
 
