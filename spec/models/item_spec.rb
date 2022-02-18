2100287a05c8bbc43d74e5e4a25292a8b1410d4d
hf-since2021 <hrk.fj.0911mon@gmail.com>
2022-02-18 19:00:59 +0900

    Itemモデルの単体テスト

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,77 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品機能' do
+    context '商品の出品ができるとき' do
+      it 'ログイン・8項目入力・画像添付で登録できる' do
+        expect(@item).to be_valid
+      end
+    end
+    context '商品の出品ができないとき' do
+      it '商品画像がないと登録できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Image can't be blank"
+      end
+      it '商品名がないと登録できない' do
+        @item.name = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Name can't be blank"
+      end
+      it '商品の説明がないと登録できない' do
+        @item.information = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Information can't be blank"
+      end
+      it 'カテゴリーが初期値だと登録できない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Category can't be blank"
+      end
+      it '商品の状態の情報が初期値だと登録できない' do
+        @item.sales_status_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Sales status can't be blank"
+      end
+      it '配送料の負担の情報が初期値だと登録できない' do
+        @item.shipping_fee_status_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Shipping fee status can't be blank"
+      end
+      it '発送元の地域の情報がないと登録できない' do
+        @item.prefecture_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Prefecture can't be blank"
+      end
+      it '発送までの日数の情報がないと登録できない' do
+        @item.scheduled_delivery_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Scheduled delivery can't be blank"
+      end
+      it '価格の情報がないと登録できない' do
+        @item.price = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price can't be blank"
+      end
+      it '価格は¥300未満だと登録できない' do
+        @item.price = Faker::Number.between(from: 0, to: 300 - 1)
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price must be greater than or equal to 300"
+      end
+      it '価格は¥10,000,00以上だと登録できない' do
+        @item.price = 9999999 + 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price must be less than or equal to 9999999"
+      end
+      it '価格は半角数値以外だと登録できない' do
+        @item.price = @item.price.to_s.tr('0-9','０-９')
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price is not a number"
+      end
+    end
+  end
 end
