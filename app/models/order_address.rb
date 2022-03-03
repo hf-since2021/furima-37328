f1133964e94b8087dcd389c384acdd2219a9d156
hf-since2021 <hrk.fj.0911mon@gmail.com>
2022-03-03 16:45:08 +0900

    クレジット決済機能実装

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,15 +1,17 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :token
 
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :postal_code, format:
+              { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :city
     validates :house_number
     validates :phone_number,
               format: { with: /\A[0-9]{10,11}\z/, message: 'is invalid. It must consist of 10 digits or 11 digits' }
+    validates :token
   end
 
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
