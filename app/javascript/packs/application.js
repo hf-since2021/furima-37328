b8b4484e3e9f7461bd4d42001f556d11387cd951
hf-since2021 <hrk.fj.0911mon@gmail.com>
2023-01-30 12:01:26 +0900

    PAY.JPをV2対応へ修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,8 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 require("../item")
-require("../card")
+// require("../card")
+require("../card2")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
